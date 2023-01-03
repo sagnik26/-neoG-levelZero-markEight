d88840b65c1e5512d2b6636cdb3649b2ae279749
sagnik26 <sagnikjohnson26@gmail.com>
2023-01-03 16:13:51 +0530

    All features added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
-import logo from './logo.svg';
 import './App.css';
+import './styles.css'
+import { useState } from "react"
+
+const emojiDictionary = {
+  "😊" : "smiling",
+  "😯" : "disbelief",
+  "😞" : "sad",
+  "😑" : "annoyance",
+  "💕" : "love",
+  "😄" : "happy"
+}
+
+var emojisWeKnow =  Object.keys(emojiDictionary)
 
 function App() {
+  const [meaning, setMeaning] = useState("")
+
+  function emojiInputHandler(event) {
+    var userInput = event.target.value
+    var meaning = emojiDictionary[userInput]
+    setMeaning(meaning)
+    if(meaning === undefined) {
+      meaning = "We don't know this"
+    }
+  }
+
+  function emojiClickHandler(emoji) {
+    var meaning = emojiDictionary[emoji]
+    setMeaning(meaning)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Emoji Interpreter</h1>
+      <input onChange={emojiInputHandler}/>
+
+      <h2>{meaning}</h2>
+
+      <h3>emojis we know</h3>
+      {
+        emojisWeKnow.map(emoji => {
+          return (
+            <span 
+              onClick={() => emojiClickHandler(emoji)}
+              style={{ fontSize: "2rem", padding: "0.5rem", cursor: "pointer" }} 
+              key={emoji}
+            > 
+              {emoji} 
+            </span>
+          )
+        })
+      }
     </div>
   );
 }
